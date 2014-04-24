94e0f532399f7c2f44311f128c32ab68702831ba
oblivion <martin.groenholdt@gmal.com>
2014-04-24 23:38:41 +0000

    Added logging.

--- ssg/__init__.py
+++ ssg/__init__.py
@@ -1,4 +1,7 @@
+import logging
 import os
+from log import logger, init_file_log, init_console_log
+
 
 __version__ = '0.0.1'
 
@@ -7,13 +10,24 @@ def get_content_files(path, extension):
     """Get a list of files with extension from path an subdirectories."""
     filelist = list()
 
+    logger.debug('Looking in "' + path + '" for files with extension "'
+                 + extension + '"')
     for entry in os.listdir(path):
-        #Ignore . and ..
+        # Ignore . and ..
         if not (entry.find('.') == 0):
             filename = path + "/" + entry
-            #Process subdirectories
+            # Process subdirectories
             if os.path.isdir(filename):
                 filelist.append(get_content_files(filename, extension))
             else:
-                if (os.path.splitext(path) == extension):
-                    filelist.append(
+                if (os.path.splitext(filename) == extension):
+                    logger.debug("Found: " + filename)
+                    filelist.append(filename)
+
+
+def init(debug=False):
+    init_file_log(logging.DEBUG)
+    if debug:
+        init_console_log(logging.DEBUG)
+    else:
+        init_console_log(logging.INFO)
