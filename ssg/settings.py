ce5e32e17af8947bac46288b2ab063fe080343ae
deadbok <martin.groenholdt@gmail.com>
2014-04-25 22:49:12 +0200

    Basic config.py handling.

--- ssg/settings.py
+++ ssg/settings.py
@@ -3,7 +3,33 @@
 @since 23/04/2014
 @author: oblivion
 '''
+import os
+from ssg.log import logger
+from importlib.machinery import SourceFileLoader
+
 
 DEFAULT_CONFIG = {
-    'SITEURL': 'localhost'
+    'SITENAME': 'Spam',
+    'SITEURL': 'localhost',
+    'ROOTDIR': os.getcwd(),
+    'CONTENTDIR': 'content'
 }
+'''Default configuration values.'''
+
+SETTINGS = DEFAULT_CONFIG
+'''Dictionary for all configuration values.'''
+
+
+def init():
+    '''
+    Initialise settings from default values and config.py, in the current dir.
+    '''
+    try:
+        # Import config.py
+        config = SourceFileLoader('config', os.getcwd() +
+                                  '/config.py').load_module()
+        # Update settings with values from config.py
+        SETTINGS.update(config.CONFIG)
+    except FileNotFoundError as exception:
+        logger.warning("No config.py found, using default values.")
+
