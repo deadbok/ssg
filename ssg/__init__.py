a330fca31a30fbca7cbb34faf15be2f9b0f2c4d7
deadbok <martin.groenholdt@gmail.com>
2014-04-25 19:11:14 +0200

    Mardown to HTML conversion works.

--- ssg/__init__.py
+++ ssg/__init__.py
@@ -1,9 +1,17 @@
 import logging
 import os
-from log import logger, init_file_log, init_console_log, close_log
+import markdown
+from ssg.log import logger, init_file_log, init_console_log, close_log
 
 
 __version__ = '0.0.1'
+
+
+class ContentParserError(RuntimeError):
+    '''
+    The client has received an unexpected response
+    '''
+    pass
 
 
 def get_content_files(path, extension):
@@ -24,11 +32,37 @@ def get_content_files(path, extension):
             filename = path + "/" + entry
             # Process subdirectories
             if os.path.isdir(filename):
-                filelist.append(get_content_files(filename, extension))
+                filelist.extend(get_content_files(filename, extension))
             else:
-                if (os.path.splitext(filename) == extension):
+                _, ext = os.path.splitext(filename)
+                if (ext == extension):
                     logger.debug("Found: " + filename)
                     filelist.append(filename)
+    return(filelist)
+
+
+def read_metadata(file):
+    '''Read metadata from a content file.
+
+    Throws a ContentParserError exception if no metadata is found.
+
+    :param file: The file to read from.
+    :type file: file
+    :returns: The metadata.
+    '''
+    metadata = dict()
+
+    line = file.readline()
+    if not line.startswith('---'):
+        raise ContentParserError('No metadata found.')
+    # Run through lines until a line that starts with '...'
+    line = file.readline()
+    while not line.startswith('...'):
+        meta = line.split(':')
+        metadata[meta[0].strip()] = meta[1].strip()
+        line = file.readline()
+    return(metadata)
+
 
 
 def init(debug=False):
@@ -47,8 +81,23 @@ def process_content(path):
     '''Process all contents, converting it to HTML.
 
     :param path: Where the content files are at.
-    :type path: stringS
+    :type path: string
+    :returns: A list of tuples of metadata and content
     '''
+    logger.info("Processing content.")
+    contents = list()
+    # Get list of files
+    content_files = get_content_files(path, '.md')
+
+    for filename in content_files:
+        with open(filename, 'r') as markdown_file:
+            logger.debug("Reading metadata from: " + filename)
+            metadata = read_metadata(markdown_file)
+            logger.debug("Reading markdown from: " + filename)
+            md = markdown_file.read()
+            content = markdown.markdown(md, output_format='html5')
+            contents = (metadata, content)
+    return(contents)
 
 
 def close():
--- ssg/readers/yamlmeta.py
+++
@@ -1,5 +0,0 @@
-'''
-
-@since 23/04/2014
-@author: oblivion
-'''
