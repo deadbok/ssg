2b06ea0a45327e06938dd8b9269408c07122dd2b
deadbok <martin.groenholdt@gmail.com>
2014-05-28 11:17:31 +0200

    Changed category parser.

--- ssg/metaext/categorymetaparser.py
+++ ssg/metaext/categorymetaparser.py
@@ -3,9 +3,14 @@ CategoryMetaParser
 ==================
 
 Meta data parser to extract category data from content directories. Each
-directory under the content path, becomes a category. E.g.
-``python/ssg/somefile.md``, creates "somefile" in the category "ssg" under the
-category "python".
+directory under the content path, becomes a sub- or category. The parser
+handles the last directory differently, according to these rules:
+
+- If there are multiply content files in the directory, the name of the
+  directory is interpreted as a category.
+
+- If there is only one content file, the directory name is interpreted as the
+  title of the content and is discarded.
 
 Reserved meta data keywords
 ---------------------------
@@ -20,13 +25,14 @@ import os
 from ssg.log import logger
 from ssg.settings import SETTINGS
 from ssg import metadata
+from ssg.tools import get_files
 
 
 class CategoryMetaParser(metadata.MetaParserBase):
     '''Get category meta data from directory names.'''
     def __init__(self):
         '''Constructor.'''
-        pass
+        metadata.MetaParserBase.__init__(self)
 
     def parse(self, path):
         '''Parse category from path and return meta data in a dictionary.
@@ -36,6 +42,10 @@ class CategoryMetaParser(metadata.MetaParserBase):
         :returns: dict with meta data.
         '''
         logger.debug("Parsing category.")
+        # Get content directory
+        content_dir, _ = os.path.split(path)
+        # Get content in directory
+        content_files = get_files(content_dir, '*.md')
         # Get content path
         content_path = os.path.join(SETTINGS['ROOTDIR'],
                                     SETTINGS['CONTENTDIR'])
@@ -45,15 +55,30 @@ class CategoryMetaParser(metadata.MetaParserBase):
         relpath, _ = os.path.split(relpath)
         # Create category for each sub directory
         category = relpath.split('/')
-        logger.debug("Category: " + str(category))
-        # Create dictionary for the meta data
-        result = dict()
-        # Set category to 'None' if no sub directory
-        if category == '':
-            result['category'] = 'None'
+
+        if len(content_files) > 1:
+            # Multiply content files, treat last directory as a category
+            logger.debug("Category: " + str(category))
+            # Create dictionary for the meta data
+            result = dict()
+            # Set category to 'None' if no sub directory
+            if category == '':
+                result['category'] = 'None'
+            else:
+                result['category'] = category
+            return result
         else:
-            result['category'] = category
-        return(result)
+            # Single content file, treat last directory as a title
+            del category[-1]
+            logger.debug("Category: " + str(category))
+            # Create dictionary for the meta data
+            result = dict()
+            # Set category to 'None' if no sub directory
+            if category == '':
+                result['category'] = 'None'
+            else:
+                result['category'] = category
+            return result
 
 # Add CategoriMetaParser to list of parsers
 metadata.META_PARSERS.append(CategoryMetaParser())
