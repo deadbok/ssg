542179f867c1cf8f17b33e90e43391d9eeebe369
deadbok <martin.groenholdt@gmail.com>
2015-02-27 11:02:11 +0100

    Better error handling in localurl filter.

--- ssg/contentfilters/localurl.py
+++ ssg/contentfilters/localurl.py
@@ -34,8 +34,13 @@ class LocalURL(contentfilter.ContentFilterBase):
         logger.debug('Running LocalURL extension.')
         localURL, _ = os.path.split(content['metadata']['URL'])
         logger.debug('Local URL: ' + localURL)
-        tmpl = Template(content['content'])
-        content['content'] = tmpl.substitute(LOCALURL=localURL)
+        try:
+            tmpl = Template(content['content'])
+            content['content'] = tmpl.substitute(LOCALURL=localURL)
+        except KeyError as exception:
+            logger.error('Could not find key ' + str(exception) + '.')
+            logger.error('Maybe a missing $ character.')
+            raise exception 
 
 # Add LocalURL to list of parsers
 contentfilter.CONTENTFILTERS.append(LocalURL())
